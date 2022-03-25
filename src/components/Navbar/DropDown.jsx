eff865f7d36034986443e2c99e2a0147ed96515d
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-03-24 19:29:29 -0700

    DropDown component now maps over array of obj from DropDownMenuItems

--- src/components/Navbar/DropDown.jsx
+++ src/components/Navbar/DropDown.jsx
@@ -1,16 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { DropDownMenu, DropListItem, DropLink } from './DropDownElements';
+import { MenuItems } from './DropDownMenuItems';
 
 function DropDown() {
+  
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    console.log("clicked")
+    setIsOpen((prevOpen) => !prevOpen)
+  }
+
   return (
-    <div>
-      <ul>
-        <li>general classification</li>
-        <li>Links</li>
-        <li>Gallery</li>
-        <li>Video</li>
-        <li>Contact</li>
-      </ul>
-    </div>
+<>
+
+    <DropDownMenu isOpen={isOpen} toggle={toggle}>
+    {MenuItems.map((item, index) => {
+      return (
+        <DropListItem key={index}>
+          <DropLink
+            id={item.cName}
+            to={item.path}
+            onClick={toggle}
+          >
+            {item.title}
+          </DropLink>
+        </DropListItem>
+      )
+    })}
+
+    </DropDownMenu>
+</>
+    // <div>
+    //   <ul>
+    //     <li>general classification</li>
+    //     <li>Links</li>
+    //     <li>Gallery</li>
+    //     <li>Video</li>
+    //     <li>Contact</li>
+    //   </ul>
+    // </div>
   )
 }
 
