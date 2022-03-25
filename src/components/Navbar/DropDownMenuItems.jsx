19babff6e229183133e4e6a43c73b7963c2bb83a
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-03-24 18:43:56 -0700

    Array of objects to be rendered in Dropdown menu

---
+++ src/components/Navbar/DropDownMenuItems.jsx
@@ -0,0 +1,28 @@
+export const MenuItems = [
+  {
+    title: 'General Classifications',
+    path: '/gc',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Links',
+    path: '/links',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Gallery',
+    path: '/gallery',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Video',
+    path: '/video',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Contact',
+    path: '/contact',
+    cName: 'dropdown-link'
+  },
+
+]
