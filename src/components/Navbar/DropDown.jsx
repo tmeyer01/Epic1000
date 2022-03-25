c24ce5240daf7533b80dfb0446111d6d821a210c
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-03-24 18:35:28 -0700

    Created temp drop down

---
+++ src/components/Navbar/DropDown.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function DropDown() {
+  return (
+    <div>
+      <ul>
+        <li>general classification</li>
+        <li>Links</li>
+        <li>Gallery</li>
+        <li>Video</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  )
+}
+
+export default DropDown
