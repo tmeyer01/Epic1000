c3c5c02985c67f0c04d220532a01febb27af188a
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-03-24 18:36:46 -0700

    Add nav menu and links

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,36 +1,97 @@
-import {useState} from 'react';
+import { useState } from "react";
 // import { Button } from '../Button/index'
 // import { Dropdown } from './DropDown'
 import { GiMountainRoad } from "react-icons/gi";
-import {FaBars} from 'react-icons/fa';
+import { BsFillCaretDownFill } from "react-icons/bs";
+import { IconContext } from "react-icons";
+import { FaBars, FaTimes } from "react-icons/fa";
+// import {FaTimes} from 'react-icons/fa'
+import Dropdown from "./DropDown";
 
-import { Nav, NavBarContainer, NavLogo, MobileIcon } from './NavElements'
-
+import {
+  Nav,
+  NavBarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+} from "./NavElements";
 
 const Navbar = () => {
-  
   const [isOpen, setIsOpen] = useState(false);
-  
+  const [dropdown, setDropdown] = useState(false);
+
   const toggle = () => {
-    console.log("clicked")
-    setIsOpen((prevOpen) => !prevOpen)
-  }
-  
+    console.log("clicked");
+    console.log("isOpen", isOpen);
+    setIsOpen((prevOpen) => !prevOpen);
+  };
+
+  const closeMobileMenu = () => setIsOpen(false);
+
+  const onMouseEnter = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(true);
+    }
+  };
+
+  const onMouseLeave = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(false);
+    }
+  };
+
   return (
     <>
-    <Nav>
-    <NavLogo to="/">
-      BC EPIC 1000
-      <GiMountainRoad />
-    </NavLogo>
-    <MobileIcon onClick={toggle}>
-      <FaBars />
-    </MobileIcon>
-
-
-    </Nav>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <Nav>
+          <NavBarContainer>
+          <NavLogo to="/">
+            BC EPIC 1000
+            <GiMountainRoad />
+          </NavLogo>
+         
+          <MobileIcon onClick={toggle}>
+            {isOpen ? <FaBars /> : <FaTimes />}
+          </MobileIcon>
+          
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="/about">About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="/Route">Route</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="/Rules">Rules</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="/Register">Register</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="/Forum">Forum</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="/Buckshopt">Buckshot</NavLinks>
+            </NavItem>
+            <NavItem onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+              <NavLinks to="/More" onClick={closeMobileMenu}>
+                More
+                <BsFillCaretDownFill />
+              </NavLinks>
+              {dropdown && <Dropdown />}
+            </NavItem>
+          </NavMenu>
+          </NavBarContainer>
+        </Nav>
+      </IconContext.Provider>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
