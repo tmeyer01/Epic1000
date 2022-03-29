33188cf7cbb8450d1fbb076a884cf7addef03610
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-03-28 18:59:10 -0700

    added btn wrapper

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,23 +1,25 @@
-import {useState} from 'react';
-import Video from '../../videos/forest.mp4'
-import Button from '../Button'
-import logo from '../../images/logoWithBlackOutline.png'
+import { useState } from "react";
+import Video from "../../videos/forest.mp4";
+import Button from "../Button";
+import logo from "../../images/logoWithBlackOutline.png";
 
 function index() {
   return (
     <HeroContainer>
       <HeroBackGround>
-        <VideoBackGround autoPlay loop muted src={Video} type='video/mp4'  />
+        <VideoBackGround autoPlay loop muted src={Video} type="video/mp4" />
       </HeroBackGround>
       <HeroContent>
-        <HeroImg alt='BCepic logo' src={logo} />
+        <HeroImg alt="BCepic logo" src={logo} />
         <HeroH1>Grand Depart June 25th 2022</HeroH1>
-        <Button to='/Signup' onMouseEnter={onHover} onMouseLeave={onHover}>
+        <HeroBtnWrapper>
+          <Button to="/Signup" onMouseEnter={onHover} onMouseLeave={onHover}>
             Register {hover ? <ArrowForward /> : <ArrowRight />}
-        </Button>
+          </Button>
+        </HeroBtnWrapper>
       </HeroContent>
     </HeroContainer>
-  )
+  );
 }
 
-export default index
+export default index;
