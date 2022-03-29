ade5c33a316479ce353725bab311ee0abdaf5ca4
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-03-28 19:55:02 -0700

    linitng

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,19 +1,38 @@
 import { useState } from "react";
 import Video from "../../videos/forest.mp4";
-import Button from "../Button";
+import {Button} from "../Button/index.jsx";
 import logo from "../../images/logoWithBlackOutline.png";
 
-function index() {
+import {
+  HeroContainer,
+  HeroBackGround,
+  VideoBackGround,
+  HeroContent,
+  HeroImg,
+  HeroH1,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight
+} from './HeroElements'
+
+const Hero = () => {
+
+  const[hover, setHover] = useState(false)
+
+  const onHover = () => {
+    setHover((prev)=> !prev)
+  }
+
   return (
     <HeroContainer>
       <HeroBackGround>
         <VideoBackGround autoPlay loop muted src={Video} type="video/mp4" />
       </HeroBackGround>
       <HeroContent>
-        <HeroImg alt="BCepic logo" src={logo} />
+        <HeroImg alt="logo" src={logo} />
         <HeroH1>Grand Depart June 25th 2022</HeroH1>
         <HeroBtnWrapper>
-          <Button to="/Signup" onMouseEnter={onHover} onMouseLeave={onHover}>
+          <Button to="/Register" onMouseEnter={onHover} onMouseLeave={onHover}>
             Register {hover ? <ArrowForward /> : <ArrowRight />}
           </Button>
         </HeroBtnWrapper>
@@ -22,4 +41,4 @@ function index() {
   );
 }
 
-export default index;
+export default Hero;
