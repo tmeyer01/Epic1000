f0676b1c3e0cdf502404fcfc8717c4632bb3f144
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-03-28 17:25:33 -0700

    changed link routes

--- src/components/MobileSideBar/index.jsx
+++ src/components/MobileSideBar/index.jsx
@@ -19,16 +19,16 @@ function MobileSideBar({isOpen, toggle}) {
       </Icon>
       <SidebarWrapper>
         <SidebarMenu>
-          <SidebarLink to="/SignUp"onClick={toggle}>About</SidebarLink>
-          <SidebarLink to="/SignUp"onClick={toggle}>Rules</SidebarLink>
-          <SidebarLink to="/SignUp"onClick={toggle}>Register</SidebarLink>
-          <SidebarLink to="/SignUp"onClick={toggle}>Forum</SidebarLink>
-          <SidebarLink to="/SignUp"onClick={toggle}>Buckshot</SidebarLink>
-          {/* <SidebarLink></SidebarLink> */}
-          <SidebarLink to="/SignUp"onClick={toggle}>About</SidebarLink>
-          <SidebarLink to="/SignUp"onClick={toggle}>About</SidebarLink>
-          <SidebarLink to="/SignUp"onClick={toggle}>About</SidebarLink>
-          <SidebarLink to="/SignUp"onClick={toggle}>About</SidebarLink>
+          <SidebarLink to="/About"onClick={toggle}>About</SidebarLink>
+          <SidebarLink to="/Rules"onClick={toggle}>Rules</SidebarLink>
+          <SidebarLink to="/Register"onClick={toggle}>Register</SidebarLink>
+          <SidebarLink to="/Forum"onClick={toggle}>Forum</SidebarLink>
+          <SidebarLink to="/BuckShot"onClick={toggle}>Buckshot</SidebarLink>
+        
+          <SidebarLink to="/GC"onClick={toggle}>General Classifications</SidebarLink>
+          <SidebarLink to="/Links"onClick={toggle}>Links</SidebarLink>
+          <SidebarLink to="/Gallery"onClick={toggle}>Gallery</SidebarLink>
+          <SidebarLink to="/Video"onClick={toggle}>Video</SidebarLink>
 
 
         </SidebarMenu>
