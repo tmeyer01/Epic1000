2b16debb9f2ca6fa6593e6828f5f00d4d8949147
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-03-28 18:54:56 -0700

    Added Hero elments

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import {useState} from 'react';
+import Video from '../../videos/forest.mp4'
+import Button from '../Button'
+import logo from '../../images/logoWithBlackOutline.png'
 
 function index() {
   return (
-    <div>index</div>
+    <HeroContainer>
+      <HeroBackGround>
+        <VideoBackGround autoPlay loop muted src={Video} type='video/mp4'  />
+      </HeroBackGround>
+      <HeroContent>
+        <HeroImg alt='BCepic logo' src={logo} />
+        <HeroH1>Grand Depart June 25th 2022</HeroH1>
+        <Button to='/Signup' onMouseEnter={onHover} onMouseLeave={onHover}>
+            Register {hover ? <ArrowForward /> : <ArrowRight />}
+        </Button>
+      </HeroContent>
+    </HeroContainer>
   )
 }
 
