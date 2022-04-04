d9d538951c9edee7b249b2678cad2efcd42317a3
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-04-04 12:23:41 -0700

    init commit for Rules Page

---
+++ src/pages/Rules.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Rules from '../components/Rules'
+import Footer from '../components/Footer'
+
+
+function RulesPage() {
+  return (
+   <>
+   <Rules/>
+   <Footer/>
+   </>
+  )
+}
+
+export default RulesPage
