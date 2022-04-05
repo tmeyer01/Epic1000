6441e050998926a5864a708257c3cf3afe371590
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-04-05 09:41:52 -0700

    temp not sure if will be used

---
+++ src/components/DropDownMenu/DropDownRouteItems.jsx
@@ -0,0 +1,28 @@
+export const RouteItems = [
+  {
+    title: 'Route Map',
+    path: '/map',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Okanagan Section',
+    path: '/okanagan-section',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Boundary Section',
+    path: '/boundary-section',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'West Kootenay Section',
+    path: '/west-kootenay-section',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'East Kootenay Section',
+    path: '/east-kootenay-section',
+    cName: 'dropdown-link'
+  },
+
+]
