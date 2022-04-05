febdb7bb75a2ce66d885b1d89f33633eba2cfaef
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-04-05 12:25:21 -0700

    init commit of route page

---
+++ src/pages/Route.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Route from '../components/Route'
+import Footer from '../components/Footer'
+
+
+function RulesPage() {
+  return (
+   <>
+   <Route/>
+   <Footer/>
+   </>
+  )
+}
+
+export default RulesPage
