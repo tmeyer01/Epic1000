234472ff7534fcd7ed0824600fd47c00cd8037cf
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-04-05 17:26:05 -0700

    linting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from "react";
-import MobileSideBar from '../components/MobileSideBar/index'
-import Nav from '../components/Navbar'
+// import React, { useEffect, useState } from "react";
+// import MobileSideBar from '../components/MobileSideBar/index'
+// import Nav from '../components/Navbar'
 import Hero from '../components/Hero'
-import About from '../components/About'
-import Footer from '../components/Footer'
-
+// import About from '../components/About'
+// import Footer from '../components/Footer'
+import { motion } from "framer-motion";
 
 const Home = () => {
 
@@ -43,9 +43,18 @@ const Home = () => {
     {/* <MobileSideBar isOpen={isOpen} toggle={toggle}/>
     <Nav toggle={toggle} isOpen={isOpen}/> */}
     {/* <div className="sections"> */}
+
+    <motion.div
+    animate={{opacity: 1}}
+    initial={{opacity: 0}}
+    exit={{opacity: 0}}
+    transition={{ duration: 0.2}}
+    >
       <Hero />
+
+      </motion.div>
       {/* <About /> */}
-      <Footer/>
+      {/* <Footer/> */}
     {/* </div> */}
   </>
   )
