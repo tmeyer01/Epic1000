d52c7673f6867dcf07052c43b0d8eddeb1b44eba
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-04-19 14:25:44 -0700

    Created header data

---
+++ src/data/headerData.jsx
@@ -0,0 +1,16 @@
+export const headerAbout = {
+  title: `About`,
+  bgImage: require('../images/about.jpeg')
+}
+
+export const headerRoute = { 
+  title: `Route`,
+  bgImage: require('../images/route.jpeg')
+
+}
+
+export const headerRules = { 
+  title: `Rules`,
+  bgImage: require('../images/R2rules.jpeg')
+
+}
