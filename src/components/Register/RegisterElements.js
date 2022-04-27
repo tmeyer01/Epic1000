eb73b605fadac4d2e7bccbcf43b7ba2fa10c206d
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-04-27 12:51:47 -0700

    refactor styles

--- src/components/Register/RegisterElements.js
+++ src/components/Register/RegisterElements.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import topo from "../../images/topoBackground.jpg";
+import topoDark from "../../images/topoBackground2.jpg";
 
 export const RegisterContainer = styled.div`
   background: url(${topo});
@@ -14,36 +15,112 @@ export const RegisterContainer = styled.div`
   z-index: 1;
 `;
 export const RegisterFormWrapper = styled.form`
-  width: 70%;
-  height: 70%;
+  background: url(${topoDark});
+  max-width: 860px;
+  /* height: 70%; */
+  border-radius: 15px;
+  margin: 50px 20px;
+  padding: 20px 20px;
+  color: #fff;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-evenly;
+  margin-top: 5em;
+  margin-bottom: 5em;
+
+
+`;
+
+export const RegisterFormH1 = styled.h1`
+  /* max-width: 440px; */
+  margin: 6px 6px;
+  padding: 10px;
+  font-size: 18px;
+  line-height: 26px;
+  word-wrap: break-word;
+
+  @media screen and (max-width: 786px) {
+    font-size: 15px;
+  }
+
+  @media screen and (max-width: 480px) {
+    font-size: 20px;
+  }
+`;
+
+export const RegisterFormP = styled.p`
+  /* max-width: 440px; */
+  margin: 6px 6px;
+  padding: 10px;
+  font-size: 18px;
+  line-height: 26px;
+  word-wrap: break-word;
+
+  @media screen and (max-width: 786px) {
+    font-size: 15px;
+  }
+
+  @media screen and (max-width: 480px) {
+    font-size: 20px;
+  }
 `;
 
 export const InputName = styled.input`
   width: 300px;
   height: 30px;
   font-size: 14px;
-  border: 3px solid black;
+
+  border: none;
+  padding: 20px 15px 20px 15px;
   border-radius: 5px;
+  margin-bottom: 10px;
 `;
 export const InputEmail = styled.input`
   width: 300px;
   height: 30px;
   font-size: 14px;
-  border: 3px solid black;
+
+  border: none;
+  padding: 20px 15px 20px 15px;
   border-radius: 5px;
+  margin-bottom: 10px;
 `;
 
 export const SentButton = styled.button`
   width: 150px;
   height: 50px;
-  color: white;
-  background-color: black;
+  color: black;
+  background-color: white;
   border: none;
   border-radius: 10px;
   font-weight: 500;
   cursor: pointer;
+`;
+
+export const RegisterSubmitWriteUp = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 860px;
+
+  /* margin-top: 20px; */
+  padding: 20px 20px;
+  /* padding-bottom: 60px; */
+  background: url(${topoDark});
+  /* background: #4946f2; */
+  border-radius: 15px;
+  margin-bottom: 5em;
+`;
+
+export const RegisterSubP = styled.p`
+  color: #fff;
+  font-size: 16px;
+  line-height: 26px;
+  font-weight: 700;
+  letter-spacing: 1.4px;
+  word-wrap: break-word;
+  /* text-transform: uppercase; */
+  margin: 6px 6px;
 `;
--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -1,17 +1,48 @@
-import React from 'react'
+import React from "react";
+import {
+  RegisterContainer,
+  RegisterFormWrapper,
+  RegisterFormH1,
+  RegisterFormP,
+  InputName,
+  InputEmail,
+  SentButton,
+  RegisterSubmitWriteUp,
+  RegisterSubP
+
+} from "./RegisterElements";
 
 function Register() {
   return (
-   <>
-   <RegisterContainer>
-     <RegisterFormWrapper>
-       <InputName />
-       <InputEmail />
-       <SentButton>Im in!</SentButton>
-     </RegisterFormWrapper>
-   </RegisterContainer>
-   </>
-  )
+    <>
+      <RegisterContainer>
+        <RegisterFormWrapper>
+         
+          <RegisterFormH1>Feeling like taking on the Epic in 2022!?</RegisterFormH1>
+          <RegisterFormP>  Add you name to the starter
+          list..</RegisterFormP>
+          <InputName type="text" name="from_name" placeholder="Name" />
+          <InputEmail type="text" name="reply_to" placeholder="Email" />
+          <SentButton>Im in!</SentButton>
+        </RegisterFormWrapper>
+
+        <RegisterSubmitWriteUp>
+          <RegisterSubP>
+
+        Once submitted, you will get an email the same or within the next bussiness day to get some more info and confirm your intention to ride. 
+          </RegisterSubP>
+          <RegisterSubP>
+          PLEASE CHECK YOUR SPAM FOLDER IF YOU DID NOT RECEIVE A CONFIRMATION EMAIL WITHIN 48 HOURS.  
+          </RegisterSubP>
+          <RegisterSubP>
+          Once replied your name will be added to the challenger list below.
+          </RegisterSubP>
+        </RegisterSubmitWriteUp>
+
+
+      </RegisterContainer>
+    </>
+  );
 }
 
-export default Register
+export default Register;
